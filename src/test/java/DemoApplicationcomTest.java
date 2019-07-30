5f8fba71afb7573f71ca0641aeacb9ba02beabe0
Thorsten <50742079+DXCTho@users.noreply.github.com>
2019-07-30 10:46:18 +0200

    Update and rename DemoApplicationcom.java to DemoApplicationcomTest.java

--- src/test/java/DemoApplicationcomTest.java
+++ src/test/java/DemoApplicationcomTest.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplicationcom {
+public class DemoApplicationcomTest {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplicationcom.class, args);
