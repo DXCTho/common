5b723bb7015fc7f83613df83305880ce1fd30846
Thorsten <50742079+DXCTho@users.noreply.github.com>
2019-07-30 10:42:26 +0200

    Update DemoApplicationcom.java

--- src/main/java/DemoApplicationcom.java
+++ src/main/java/DemoApplicationcom.java
@@ -9,10 +9,4 @@ public class DemoApplicationcom {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplicationcom.class, args);
     }
-    private void johnsUnusedCodeSmellMethod() {
-       String buggy=null;
-        buggy.trim();
-        
-        System.out.println("Foo!!!!!?");
-    }
 }
